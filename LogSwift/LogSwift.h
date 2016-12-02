9dafe952be19aa0b272cbc70a63f950fd156311b
pengyucheng <pengyucheng@huoshug.local>
2016-12-02 12:22:24 +0800

    modified:   LogSwift/LogSwift.h

--- LogSwift/LogSwift.h
+++ LogSwift/LogSwift.h
@@ -15,7 +15,7 @@ FOUNDATION_EXPORT double PBBLogSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char PBBLogSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PBBLogSDK/PublicHeader.h>
-#import "LogSwift-Swift.h"
+//#import "LogSwift-Swift.h"
 
 #elif TARGET_OS_MAC
 #import <Cocoa/Cocoa.h>
