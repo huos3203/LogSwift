5623aa59ea4ec975aa4a2204c0f6205df12533b3
huos3203 <724987481@qq.com>
2019-01-30 12:28:43 +0800

    gengx

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -7,10 +7,22 @@
 //
 
 import Foundation
-
 class Fastfile: LaneFile {
-	func customLane() {
-	desc("Description of what the lane does")
+    //fastlane podPushLane version:1.2 project:LogSwift
+	func podPushLane(withOptions options:[String: String]?) {
+        desc("Description of what the lane does")
 		// add actions here: https://docs.fastlane.tools/actions
+        let target_v:String = (options?["version"])!
+        let project:String  = (options?["project"])!
+        let path:String!     = "\(project).podspec"
+        cocoapods()
+        podLibLint()
+        versionBumpPodspec(path: path, versionNumber: target_v, versionAppendix: nil)  //更新 podspec
+        
+        gitCommit(path: path, message: "Bump version to \(target_v)") //提交版本号修改
+        addGitTag(buildNumber: target_v)  //设置 tag
+        pushToGitRemote() // 推送到 git 仓库
+        //提交到 CocoaPods
+//        podPush(useBundleExec: <#T##Bool#>, path: <#T##String?#>, repo: <#T##String?#>, allowWarnings: <#T##String?#>, useLibraries: <#T##String?#>, sources: <#T##[String]?#>, swiftVersion: <#T##String?#>, verbose: <#T##Bool#>)
 	}
 }
